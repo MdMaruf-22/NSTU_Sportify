ee1b758902282b634220e79375a51e92d9be410c
Maruf <marufislam74208@gmail.com>
2024-10-20 14:29:50 +0600

    Rearranged and reorganized sports models

--- backend/nstu_sportify_backend/sports/models.py
+++ backend/nstu_sportify_backend/sports/models.py
@@ -1,99 +1,5 @@
 from django.db import models
 
-class Standing(models.Model):
-    standing_id = models.AutoField(primary_key=True)
-    position = models.IntegerField()
-    team = models.ForeignKey('Team', on_delete=models.CASCADE)
-    event = models.ForeignKey('Event',on_delete=models.CASCADE)
-    def __str__(self):
-        return f"{self.team.name} position {self.position} in event {self.event.title}"
-    
-class Result(models.Model):
-    result_id = models.AutoField(primary_key=True)
-    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)
-    winner_team = models.ForeignKey(Team, related_name='winner_team', on_delete=models.CASCADE)
-    loser_team = models.ForeignKey(Team, related_name='loser_team', on_delete=models.CASCADE)
-    draw = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"Result of match {self.match.match_id}"
-
-class Livescore(models.Model):
-    score_id = models.AutoField(primary_key=True)
-    score = models.IntegerField()
-    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"Livescore for match {self.match.match_id}"
-
-class Cricket(models.Model):
-    cricket_match_id = models.AutoField(primary_key=True)
-    overs = models.IntegerField()
-    runs_team1 = models.IntegerField()
-    runs_team2 = models.IntegerField()
-    wickets_team1 = models.IntegerField()
-    wickets_team2 = models.IntegerField()
-    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"Cricket Match {self.cricket_match_id}"
-
-class Notice(models.Model):
-    notice_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    posted_date = models.DateField()
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-    
-class Chess(models.Model):
-    chess_match_id = models.AutoField(primary_key=True)
-    moves = models.IntegerField()  
-    duration = models.CharField()  
-    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)  
-    def __str__(self):
-        return f"Chess Match {self.chess_match_id}"
-
-class Football(models.Model):
-    football_match_id = models.AutoField(primary_key=True)
-    goals_team1 = models.IntegerField()  
-    goals_team2 = models.IntegerField()  
-    duration = models.IntegerField()  
-    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)  
-    def __str__(self):
-        return f"Football Match {self.football_match_id}"
-
-class Handball(models.Model):
-    handball_match_id = models.AutoField(primary_key=True)
-    goals_team1 = models.IntegerField()  
-    goals_team2 = models.IntegerField()  
-    duration = models.IntegerField()  
-    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE) 
-    def __str__(self):
-        return f"Handball Match {self.handball_match_id}"
-    
-class Carrom(models.Model):
-    carrom_match_id = models.AutoField(primary_key=True)
-    rounds = models.IntegerField()  
-    points_team1 = models.IntegerField()
-    points_team2 = models.IntegerField()
-    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)  
-    def __str__(self):
-        return f"Carrom Match {self.carrom_match_id}"
-    
-class Matchdetails(models.Model):
-    match_id = models.AutoField(primary_key=True)
-    date = models.DateField()
-    location = models.CharField(max_length=255)
-    event = models.ForeignKey('Event', on_delete=models.CASCADE)  # Reference to the event
-    
-    def __str__(self):
-        return f"Match {self.match_id} on {self.date} on {self.location}"
-    
-    
 # event, player, team, representative, department
 class Department(models.Model):
     department_id = models.AutoField(primary_key=True)
@@ -140,6 +46,100 @@ class Event(models.Model):
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Matchdetails(models.Model):
+    match_id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    location = models.CharField(max_length=255)
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)  # Reference to the event
+    
+    def __str__(self):
+        return f"Match {self.match_id} on {self.date} on {self.location}"
+    
+
+class Chess(models.Model):
+    chess_match_id = models.AutoField(primary_key=True)
+    moves = models.IntegerField()  
+    duration = models.CharField()  
+    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)  
+    def __str__(self):
+        return f"Chess Match {self.chess_match_id}"
+
+class Football(models.Model):
+    football_match_id = models.AutoField(primary_key=True)
+    goals_team1 = models.IntegerField()  
+    goals_team2 = models.IntegerField()  
+    duration = models.IntegerField()  
+    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)  
+    def __str__(self):
+        return f"Football Match {self.football_match_id}"
+
+class Handball(models.Model):
+    handball_match_id = models.AutoField(primary_key=True)
+    goals_team1 = models.IntegerField()  
+    goals_team2 = models.IntegerField()  
+    duration = models.IntegerField()  
+    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE) 
+    def __str__(self):
+        return f"Handball Match {self.handball_match_id}"
+    
+class Carrom(models.Model):
+    carrom_match_id = models.AutoField(primary_key=True)
+    rounds = models.IntegerField()  
+    points_team1 = models.IntegerField()
+    points_team2 = models.IntegerField()
+    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)  
+    def __str__(self):
+        return f"Carrom Match {self.carrom_match_id}"
+
+class Standing(models.Model):
+    standing_id = models.AutoField(primary_key=True)
+    position = models.IntegerField()
+    team = models.ForeignKey('Team', on_delete=models.CASCADE)
+    event = models.ForeignKey('Event',on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.team.name} position {self.position} in event {self.event.title}"
+    
+class Result(models.Model):
+    result_id = models.AutoField(primary_key=True)
+    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)
+    winner_team = models.ForeignKey(Team, related_name='winner_team', on_delete=models.CASCADE)
+    loser_team = models.ForeignKey(Team, related_name='loser_team', on_delete=models.CASCADE)
+    draw = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Result of match {self.match.match_id}"
+
+class Livescore(models.Model):
+    score_id = models.AutoField(primary_key=True)
+    score = models.IntegerField()
+    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Livescore for match {self.match.match_id}"
+
+class Cricket(models.Model):
+    cricket_match_id = models.AutoField(primary_key=True)
+    overs = models.IntegerField()
+    runs_team1 = models.IntegerField()
+    runs_team2 = models.IntegerField()
+    wickets_team1 = models.IntegerField()
+    wickets_team2 = models.IntegerField()
+    match = models.ForeignKey(Matchdetails, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Cricket Match {self.cricket_match_id}"
+
+class Notice(models.Model):
+    notice_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    posted_date = models.DateField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
