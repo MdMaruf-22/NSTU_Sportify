5798ff52fd49a300de8cb96aae178238b195ea26
Maruf <marufislam74208@gmail.com>
2024-11-09 00:09:37 +0600

    Added the model to Django Dmin panel

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+^
+.backend/nstu_sportify_backend/sports/models.py,9\6\967336d5cdcee577689c6fe408d6a313a020e190
+c
+3backend/nstu_sportify_backend/sports/serializers.py,1\3\13d2ed012b6a7473d1a65f7027a8899c3a273ef1
+]
+-backend/nstu_sportify_backend/sports/admin.py,b\2\b2818712ee8758c8e207e4c52151bdaa98b63310
---
+++ backend/nstu_sportify_backend/sports/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Department)
+admin.site.register(Representative)
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Event)
+admin.site.register(Matchdetails)
+admin.site.register(Carrom)
+admin.site.register(Handball)
+admin.site.register(Chess)
+admin.site.register(Football)
+admin.site.register(Cricket)
+admin.site.register(Livescore)
+admin.site.register(Result)
+admin.site.register(Standing)
+admin.site.register(Notice)
