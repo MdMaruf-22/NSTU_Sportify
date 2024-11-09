9140c60d63b19681d4aa5c5eefe77b2887ea89c7
shafi <shariyarshafi27@gmail.com>
2024-11-09 13:12:10 +0600

    Added serializer for cricket,carrom,football,handball and chess

--- backend/nstu_sportify_backend/sports/serializers.py
+++ backend/nstu_sportify_backend/sports/serializers.py
@@ -25,3 +25,33 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+        from rest_framework import serializers
+
+class CarromSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Carom
+        fields = '__all__'
+
+
+class HandballSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Handball
+        fields = '__all__'
+
+
+class ChessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chess
+        fields = '__all__'
+
+
+class FootballSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Football
+        fields = '__all__'
+
+
+class CricketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cricket
+        fields = '__all__'
