df38785ac18bf4f0a73edb6dd2bd862b9cb3b53a
mission365 <missiondevnath901@gmail.com>
2024-11-14 09:26:05 +0600

    created chess, football, cricket, livescore, result, standing, notice views for api

--- backend/nstu_sportify_backend/sports/views.py
+++ backend/nstu_sportify_backend/sports/views.py
@@ -2,35 +2,70 @@ from rest_framework import viewsets
 from .models import *
 from .serializers import *
 
+
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+
 
 class RepresentativeViewSet(viewsets.ModelViewSet):
     queryset = Representative.objects.all()
     serializer_class = RepresentativeSerializer
 
+
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+
 
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
+
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
-class MatchDetailSet(viewsets.ModelViewSet):
+
+class MatchDetailViewSet(viewsets.ModelViewSet):
     queryset = Matchdetails.objects.all()
     serializer_class = MatchSerializer
+
 
 class CarromViewSet(viewsets.ModelViewSet):
     queryset = Carrom.objects.all()
     serializer_class = CarromSerializer
 
-class HandBall(viewsets.ModelViewSet):
+
+class HandBallViewSet(viewsets.ModelViewSet):
     queryset = Handball.objects.all()
     serializer_class = HandballSerializer
 
+class ChessViewSet(viewsets.ModelViewSet):
+    queryset = Chess.objects.all()
+    serializer_class = ChessSerializer
+
+class FootballViewSet(viewsets.ModelViewSet):
+    queryset = Football.objects.all()
+    serializer_class = FootballSerializer
+
+class CricketViewSet(viewsets.ModelViewSet):
+    queryset = Cricket.objects.all()
+    serializer_class = CricketSerializer
+
+class LiveScoreViewSet(viewsets.ModelViewSet):
+    queryset = Livescore.objects.all()
+    serializer_class = LiveScoreSerializer
+
+class ResultViewSet(viewsets.ModelViewSet):
+    queryset = Result.objects.all()
+    serializer_class = ResultSerializer
+
+class StandingViewSet(viewsets.ModelViewSet):
+    queryset = Standing.objects.all()
+    serializer_class = StandingSerializer
+
+class NoticeViewSet(viewsets.ModelViewSet):
+    queryset = Notice.objects.all()
+    serializer_class = NoticeSerializer
